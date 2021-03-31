76fa85dc137e895e3f6f961eb98a63294bdfae77
antoineRDL17 <antoine.gros1@etu.univ-grenoble-alpes.fr>
2021-03-31 11:40:11 +0200

    gif

--- Ghibli.js
+++ Ghibli.js
@@ -9,7 +9,6 @@ function recherche(){
          }
     })
 
-    var divGif = $(".bloc-gif-attente");
-    divGif.setAttribute("display","true");
+    $("#bloc-gif-attente").css({"display": "block"});
 
 }
