d10c840a8b4d9d9a5744aaa510afc0e673acb5a2
WilliamBerruyer <william.berruyer@iut2.grenoble-alpes.fr>
2021-04-02 09:16:04 +0200

    it is working

--- Ghibli.js
+++ Ghibli.js
@@ -6,11 +6,39 @@ function recherche(){
     contentType: "application/json",
     dataType: 'json',
     success: function(result){
-            console.log(result);
+
             console.log(search);
             unLoadGif();
+            var resultat = getGoodTitles(search, result);
+            console.log(resultat);
             var element = $('#bloc-resultats');
-            $(element).after('<p>'+ result[1].title + '</p>');
+            element.html("");
+            if(resultat != null){
+              resultat.forEach((res, i) => {
+                  element.append(
+                    '<div class="movie">' +
+                    '<h4>'+ resultat[i].title + '</h4>' +
+                    '<p>' + resultat[i].original_title +'<p>' +
+                    '<p>' + resultat[i].director + '<p>' +
+                    '<p>' + resultat[i].producer + '<p>' +
+                    '<p>' + resultat[i].release_date + '<p>' +
+                    '<p>' + resultat[i].running_time + '<p>' +
+                    '<p>' + resultat[i].rt_score + '<p>'
+
+
+
+                  );
+
+              });
+
+
+
+
+            }
+            else{
+            element.after('<p>'+ '( ∅ Aucun résultat trouvé )' + '</p>');
+            }
+
 
 
 
@@ -26,4 +54,33 @@ function loadGif(){
 }
 function unLoadGif(){
   $("#bloc-gif-attente").css({"display": "none"});
+}
+
+function getGoodTitles(text,result){
+  var newArray = Array();
+  var nbGood = 0;
+  const txtArray = text.split(' ');
+  const newTxtArray = Array();
+  txtArray.forEach(mot => {
+    if (mot.length>3){
+      newTxtArray.push(mot);
+    }
+  });
+  result.forEach(film => {
+    const titleArray = film.title.split(' ');
+    newTxtArray.forEach(mot => {
+      titleArray.forEach(motTitle => {
+        if (motTitle.toLowerCase() == mot.toLowerCase()){
+          nbGood=nbGood+1
+        }
+      });
+
+    });
+    if (nbGood==newTxtArray.length){
+      newArray.push(film);
+    }
+    nbGood=0;
+  });
+
+  return newArray;
 }
