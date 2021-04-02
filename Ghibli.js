94b5e0ae8834d4530e73a4c5133df62d12effef8
antoineRDL17 <antoine.gros1@etu.univ-grenoble-alpes.fr>
2021-04-02 08:21:27 +0200

    working gif

--- Ghibli.js
+++ Ghibli.js
@@ -1,18 +1,24 @@
 function recherche(){
-
   var search = $('#search-input').val();
+  loadGif();
+  $.ajax({
+    url: 'https://ghibliapi.herokuapp.com/films',
+    contentType: "application/json",
+    dataType: 'json',
+    success: function(result){
+            console.log(result);
+            console.log(search);
+            unLoadGif();
+       }
+  })
 
 
-    $.ajax({
-      url: 'https://ghibliapi.herokuapp.com/films',
-      contentType: "application/json",
-      dataType: 'json',
-      success: function(result){
-             console.log(result);
-               console.log(search);
-         }
-    })
 
-    $("#bloc-gif-attente").css({"display": "block"});
+}
 
+function loadGif(){
+  $("#bloc-gif-attente").css({"display": "block"});
+}
+function unLoadGif(){
+  $("#bloc-gif-attente").css({"display": "none"});
 }
